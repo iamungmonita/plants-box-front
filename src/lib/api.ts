b89b135ffbc9771ab5e2004aadde81c6610656a3
YanPuty <yanputy123@gmail.com>
2025-03-19 20:55:25 +0700

    Update API export default

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,2 @@
-const API_URL =
-  typeof window !== "undefined"
-    ? window.location.hostname === "localhost"
-      ? "http://localhost:4003"
-      : window.location.hostname === "192.168.1.101"
-      ? "http://192.168.1.101:4003"
-      : "https://your-production-url.com"
-    : "https://your-server-url.com"; // Fallback for SSR
-
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:4003";
 export default API_URL;
